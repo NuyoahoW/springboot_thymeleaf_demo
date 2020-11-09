b07e7886f2153599d46db34c032de19899a22fd9
nanfeng <nf@qq.com>
2020-11-09 21:55:02 +0800

    Initial commit

---
+++ src/main/java/com/offcn/springboot_thymeleaf_demo/SpringbootThymeleafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.offcn.springboot_thymeleaf_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafDemoApplication.class, args);
+    }
+
+}
